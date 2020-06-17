e25f2eacbf04c8f3aee866a0a9f09aabc65ecd65
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas-2.local>
2020-06-17 11:31:54 -0500

    modificando partes del layaut y dejando todo listo para empezar con el sistema de login

--- src/layouts/Main/components/Sidebar/components/Profile/Profile.js
+++ src/layouts/Main/components/Sidebar/components/Profile/Profile.js
@@ -27,9 +27,9 @@ const Profile = props => {
   const classes = useStyles();
 
   const user = {
-    name: 'Shen Zhi',
+    name: 'Jaime Riascos',
     avatar: '/images/avatars/avatar_11.png',
-    bio: 'Brain Director'
+    bio: 'Administrador'
   };
 
   return (
