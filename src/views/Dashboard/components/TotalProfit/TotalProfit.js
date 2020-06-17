01cdb7918f7e0b2b5f4f9d00d6c14a20b5017ec9
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas-2.local>
2020-06-17 10:56:39 -0500

    first commit

--- src/views/Dashboard/components/TotalProfit/TotalProfit.js
+++ src/views/Dashboard/components/TotalProfit/TotalProfit.js
@@ -52,7 +52,7 @@ const TotalProfit = props => {
               gutterBottom
               variant="body2"
             >
-              TOTAL PROFIT
+              GANANCIA
             </Typography>
             <Typography
               color="inherit"
