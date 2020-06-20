cd18016c2b306a909318d1fdfb0e989ecffbc676
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas-2.local>
2020-06-20 14:40:25 -0500

    creando el ui para mostrar la lista de prendas segun se van agregando en el viwe de nuevo pedido

---
+++ src/enviroment/index.js
@@ -0,0 +1,50 @@
+export const sizes = [
+    {
+        letter: "2XS",
+        number: 28
+    },
+    {
+        letter: "XS",
+        number: 30
+    },
+    {
+        letter: "S",
+        number: 32
+    },
+    {
+        letter: "M",
+        number: 34
+    },
+    {
+        letter: "L",
+        number: 36
+    },
+    {
+        letter: "XL",
+        number: 38
+    },
+    {
+        letter: "2XL",
+        number: 40
+    },
+    {
+        letter: "3XL",
+        number: 42
+    },
+    {
+        letter: "4XL",
+        number: 44
+    },
+    {
+        letter: "5XL",
+        number: 46
+    },
+    {
+        letter: "6XL",
+        number: 48
+    },
+    {
+        letter: "7XL",
+        number: 50
+    },
+]
--- src/views/NewOrder/components/ProductsInfo/ProductsInfo.js
+++ src/views/NewOrder/components/ProductsInfo/ProductsInfo.js
@@ -1,16 +1,174 @@
-import React from 'react'
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Divider,
+  Grid,
+  Button,
+  TextField
+} from '@material-ui/core';
+import { sizes } from '../../../../enviroment'
 
 
 
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
 
-const ProductsInfo = () =>{
+const ProductsInfo = props => {
+  const { className, ...rest } = props;
 
-    return(
-        <div>
-            Hola esta es la información de las prendas
-        </div>
-    )
-}
+  const classes = useStyles();
 
+  const [values, setValues] = useState({
+    firstName: 'Shen',
+    lastName: 'Zhi',
+    email: 'shen.zhi@devias.io',
+    phone: '',
+    state: 'Alabama',
+    country: 'USA'
+  });
 
-export default ProductsInfo
+  const handleChange = event => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const states = [
+    {
+      value: 'alabama',
+      label: 'Alabama'
+    },
+    {
+      value: 'new-york',
+      label: 'New York'
+    },
+    {
+      value: 'san-francisco',
+      label: 'San Francisco'
+    }
+  ];
+
+  return (
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <form
+        autoComplete="off"
+        noValidate
+      >
+        <CardHeader
+          subheader="Agrega Prendas al pedido"
+          title="Prendas"
+        />
+        <Divider />
+        <CardContent>
+          <Grid
+            container
+            spacing={3}
+          >
+            <Grid
+              item
+              md={12}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Prenda"
+                margin="dense"
+                name="name"
+                onChange={handleChange}
+                required
+                value={values.firstName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Talla"
+                margin="dense"
+                name="state"
+                onChange={handleChange}
+                required
+                select
+                // eslint-disable-next-line react/jsx-sort-props
+                SelectProps={{ native: true }}
+                value={values.state}
+                variant="outlined"
+              >
+                {sizes.map(option => (
+                  <option
+                    key={option.number}
+                    value={option.number}
+                  >
+                    {`${option.letter} (${option.number})`}
+                  </option>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Color"
+                margin="dense"
+                name="lastName"
+                onChange={handleChange}
+                required
+                value={values.lastName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={12}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Precio de venta"
+                margin="dense"
+                name="email"
+                onChange={handleChange}
+                required
+                value={values.email}
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <Button
+            color="primary"
+            variant="contained"
+          >
+            Agregar Prenda
+          </Button>
+        </CardActions>
+      </form>
+    </Card>
+  );
+};
+
+ProductsInfo.propTypes = {
+  className: PropTypes.string
+};
+
+export default ProductsInfo;
