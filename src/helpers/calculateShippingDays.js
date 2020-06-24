239a0984915ea7e76b9bc16731496acc955c0511
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas-2.local>
2020-06-24 15:25:36 -0500

    dejando el formulario listo y empezando a crear las funciones para agregar los pedidos a la base de datos

---
+++ src/helpers/calculateShippingDays.js
@@ -0,0 +1,41 @@
+import shippingRates from '../enviroment/shippingRates.json'
+
+
+
+export default function(location, totalValue,){
+
+    const coordinadoraRates = shippingRates.coordinadora
+    const tccRates = shippingRates.tcc
+
+    const locationRatesCoordinadora = findLoaction(location, coordinadoraRates)
+    const locationRatesTcc = findLoaction(location, tccRates)
+
+    if(!locationRatesTcc && !locationRatesCoordinadora){
+        return '---'
+    }
+
+
+    if(!locationRatesCoordinadora){
+        return locationRatesTcc.deliveryDays
+    }
+    if(!locationRatesTcc){
+        return locationRatesCoordinadora.deliveryDays
+    }
+
+    if(parseInt(locationRatesCoordinadora.deliveryDays) < parseInt(locationRatesTcc.deliveryDays)){
+        return locationRatesCoordinadora.deliveryDays
+    }else{
+        return locationRatesTcc.deliveryDays
+    }
+
+}
+
+
+const findLoaction = (location, rates) =>{
+    if(!location){
+        return null
+    }
+    return rates.find(elem => {
+        return (elem.city === location.city) && (elem.Department === location.department)
+    })
+}
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export { default as chartjs } from './chartjs';
 export { default as getInitials } from './getInitials';
 export { default as getCityList } from './getCityList'
+export { default as calculateShippingDays } from './calculateShippingDays'
