e94b6b21d0703d7cc93495ad1c22e36acd4f0a1f
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas-2.local>
2020-06-26 17:54:52 -0500

    creando la tarjeta para mostrar las prendas en los detalles del pedido

---
+++ src/views/OrderDetail/components/ProductList/Product.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import PropsTypes from 'prop-types'
+import {makeStyles} from '@material-ui/styles'
+import { Typography, IconButton } from '@material-ui/core'
+import { MoreVert } from '@material-ui/icons'
+
+
+const useStyle = makeStyles(theme=>({
+    root:{
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: theme.spacing(2),
+        '&:hover':{
+            background: theme.palette.action.hover
+        },
+        padding: theme.spacing(1)
+    },
+    icon:{
+        flex: 1
+    },
+    name:{
+        flex: 5
+    },
+    size:{
+        flex: 2
+    },
+    color:{
+        flex: 2
+    },
+    wholesalePice:{
+        flex: 2
+    },
+    sellPrice:{
+        flex: 2
+    }
+}))  
+
+
+const Product = props=>{
+
+    const classes = useStyle(props)
+    return(
+        <div className={classes.root}>
+            
+            <div className={classes.name}>
+                <Typography align="left" variant="h6">Cinturilla clásica 3h</Typography>
+                <Typography align="left" variant="subtitle2">Fajas Internacionales</Typography>
+            </div>
+            <div className={classes.size}>
+                <Typography align="center" variant="h6">M(34)</Typography>
+                <Typography align="center" variant="subtitle2">Talla</Typography>
+            </div>
+            <div className={classes.color}>
+                <Typography align="center" variant="h6">Negro</Typography>
+                <Typography align="center" variant="subtitle2">Color</Typography>
+            </div>
+            <div className={classes.wholesalePice}>
+                <Typography align="center" variant="h6">36.000</Typography>
+                <Typography align="center" variant="subtitle2">Por mayor</Typography>
+            </div>
+            <div className={classes.sellPrice}>
+                <Typography align="center" variant="h6">78.000</Typography>
+                <Typography align="center" variant="subtitle2">Venta</Typography>
+            </div>
+            <div className={classes.icon}>
+                <IconButton>
+                    <MoreVert/>
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+Product.prototype = {
+    className: PropsTypes.string
+}
+
+
+
+export default Product
+
--- src/views/OrderDetail/components/index.js
+++ src/views/OrderDetail/components/index.js
@@ -1,3 +1,4 @@
 export { default as AccountDetails } from './AccountDetails';
 export { default as ClientProfile } from './ClientProfile';
 export { default as HistoryCard } from './HistoryCard'
+export { default as ProductList } from './ProductList'
