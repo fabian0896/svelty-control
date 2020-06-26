4d7224b9b8322bcd2ef41216bbdfb0faf80d6f81
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas-2.local>
2020-06-26 16:20:26 -0500

    haciendo tarjeta para mostrar el historial del pedido

--- src/views/OrderDetail/components/HistoryCard/HistoryItem.js
+++ src/views/OrderDetail/components/HistoryCard/HistoryItem.js
@@ -1,20 +1,73 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles'
+import { ORDER_STATES } from '../../../../enviroment'
+
+import {
+    Typography
+} from '@material-ui/core'
+
+const useStyle = makeStyles(theme => ({
+    root: {
+        marginBottom: theme.spacing(2),
+        display: 'flex',
+        alignItems: 'center'
+    },
+    icon: props => ({
+        marginRight: theme.spacing(1),
+        background: ORDER_STATES['shippingProblems'].color,
+        padding: theme.spacing(1),
+        height: 40,
+        width: 40,
+        borderRadius: '50%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& > *': {
+            color: theme.palette.getContrastText(ORDER_STATES['shippingProblems'].color)
+        }
+    }),
+    description: {
+        flex: 1,
+        marginRight: theme.spacing(2)
+    },
+    date: {
+
+    }
+}))
 
 
 
-const HistoryItem =  props =>{
+const HistoryItem = props => {
 
+    const classes = useStyle(props)
 
-    return(
-        <div>
-            Este es un item
+    const StateIcon = ORDER_STATES['shippingProblems'].icon
+
+    return (
+        <div className={classes.root}>
+            <div className={classes.icon}>
+                <StateIcon />
+            </div>
+            <div className={classes.description}>
+                <Typography variant="body1">
+                    Novedade en el envio
+                </Typography>
+                <Typography variant="body2">
+                    El cliente se nego a recibir el pedido, el paquete este en terminal de destino
+                </Typography>
+            </div>
+            <div className={classes.date}>
+                <Typography color="textPrimary" variant="body2">
+                    10/24/2020
+                </Typography>
+            </div>
         </div>
     )
 }
 
 
-HistoryItem.prototype={
+HistoryItem.prototype = {
     className: PropTypes.string
 }
 
