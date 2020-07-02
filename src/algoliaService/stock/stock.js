5a218a92054c8194a27eee36db4246bb471d7c13
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas-2.local>
2020-07-02 13:22:40 -0500

    terminando view de stock

---
+++ src/algoliaService/stock/stock.js
@@ -0,0 +1,22 @@
+import algoliasearch from 'algoliasearch'
+
+const client = algoliasearch('9P7M48NHP0', 'b88ec07888dacb382644c384b79fcafa')
+const index = client.initIndex('stock')
+
+
+const add = async ({id, name, provider, size, color}) =>{
+    const algoliaObject ={
+        name,
+        provider,
+        size,
+        color,
+        objectID: id
+    }
+    const result = await index.saveObject(algoliaObject)
+    return result
+}
+
+
+export default {
+    add
+}
