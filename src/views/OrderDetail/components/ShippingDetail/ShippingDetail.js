83ec49149638abdfa16c55b4bf3fbc6e1c52c493
Fabian David Dueñas Garcia <fabian0896@outlook.com>
2020-10-12 18:46:00 -0500

    Dejando lista la pantalla para los detalles de los pedidos

--- src/views/OrderDetail/components/ShippingDetail/ShippingDetail.js
+++ src/views/OrderDetail/components/ShippingDetail/ShippingDetail.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/styles'
 
-import { 
+import {
     Card,
     CardHeader,
     Divider,
@@ -11,31 +11,32 @@ import {
 } from '@material-ui/core'
 import clsx from 'clsx'
 import { ThumbUp } from '@material-ui/icons'
+import numeral from 'numeral'
 
-const useStyle = makeStyles(theme=>({
-    root:{
+const useStyle = makeStyles(theme => ({
+    root: {
         marginTop: theme.spacing(2),
         marginBottom: theme.spacing(2)
     },
-    currierResume:{
+    currierResume: {
         marginBottom: theme.spacing(2)
     },
-    resume:{
+    resume: {
         marginBottom: theme.spacing(2),
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        '& > div':{
+        '& > div': {
             flex: 1
         }
     },
-    payResume:{
+    payResume: {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
     },
-    icon:{
+    icon: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -49,37 +50,41 @@ const useStyle = makeStyles(theme=>({
 }))
 
 
-const ShippingDetail = props =>{
-    const {className} = props
+const ShippingDetail = props => {
+    const { className, order } = props
     const classes = useStyle(props)
-    return(
+    const shipping = order.shipping || {}
+    return (
         <Card className={clsx(className, classes.root)}>
             <CardHeader
                 title="Envio"
                 subheader="Informacion del envio"
             />
-            <Divider/>
+            <Divider />
             <CardContent>
                 <div className={classes.currierResume}>
-                    <Typography align="center" variant="h3">Coordinadora</Typography>
-                    <Typography align="center" variant="subtitle2">800427802</Typography>
+                    <Typography align="center" variant="h3">{order.company_name || "No asignado"}</Typography>
+                    <Typography align="center" variant="subtitle2">{shipping.guide_number || "---"}</Typography>
                 </div>
                 <div className={classes.resume}>
                     <div>
-                        <Typography align="center" variant="h4">$15.750</Typography>
+                        <Typography align="center" variant="h4">{numeral(order.shipping_price || 0).format("$0,0")}</Typography>
                         <Typography align="center" variant="body2">valor del envio</Typography>
                     </div>
                     <div>
-                        <Typography align="center" variant="h4">Contra entrega</Typography>
+                        <Typography align="center" variant="h4">{order.paymentMethod === 'mipaquete' ? "Contra entrega" : "Consignación"}</Typography>
                         <Typography align="center" variant="body2">Modo</Typography>
                     </div>
                 </div>
-                <div className={classes.payResume}>
-                    <div className={classes.icon}>
-                        <ThumbUp color="inherit"/>
+                {
+                    order.state === "delivered" && 
+                    <div className={classes.payResume}>
+                        <div className={classes.icon}>
+                            <ThumbUp color="inherit" />
+                        </div>
+                        <Typography variant="body1" align="center">El valor ya fue cobrado y esta en la cuenta</Typography>
                     </div>
-                    <Typography variant="body1" align="center">El valor ya fue cobrado y esta en la cuenta</Typography>
-                </div>
+                }
             </CardContent>
         </Card>
     )
